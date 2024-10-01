bfc78eb479218133e017ef2249ea0899d9c2fe91
Gavriel Rudolph <gavrielmrudolph@gmail.com>
2024-10-01 18:58:00 -0400

    Remove unnecessary 'mern' folder

--- mern/client/src/components/modals/index.js
+++
@@ -1,4 +0,0 @@
-export {default as PostModal } from './PostModal';
-export { default as RegisterModal } from './RegisterModal';
-export { default as EditPostModal } from './EditPostModal';
-export { default as ModalManager } from './ModalManager';
--- mern/client/src/contexts/index.js
+++
@@ -1,5 +0,0 @@
-export { PostProvider, usePostContext } from './PostContext';
-export { PrivateModalProvider, usePrivateModalContext } from './PrivateModalContext';
-export { UserProvider, useUser, useUserUpdate } from './UserContext';
-export { NotificationProvider, useNotificationContext } from './NotificationContext';
-export { PublicModalProvider, usePublicModalContext } from './PublicModalContext';
