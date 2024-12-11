0f508e5ca01cf048b131734957f0a379b78a052a
Gamikaru <gavrielmrudolph@gmail.com>
2024-12-11 13:09:17 -0500

    feat: dark-light mode toggle impl on user dropdown, with switch to css var color syst logo switcher

--- client/src/components/nav/Logo.jsx
+++ client/src/components/nav/Logo.jsx
@@ -1,21 +1,29 @@
+import { useThemeContext } from '@contexts'; // <-- Make sure you have this exported in contexts/index.js
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Logo = React.memo(() => (
-    <div className="navbar-logo-container">
-        <Link to="/">
-            <img
-                alt="Codeblogs Website Logo"
-                aria-label="Codeblogs Logo"
-                className="nav-logo-image"
-                src="/assets/images/Icon-Only-Black.png"
-                loading="lazy"
+const Logo = React.memo(() => {
+    const { theme } = useThemeContext();
 
-            />
-        </Link>
-    </div>
-));
+    const logoSrc = theme === 'dark'
+        ? '/assets/images/Icon-Only-White.svg'
+        : '/assets/images/Icon-Only-Black.png';
 
-export default Logo;
+    return (
+        <div className="navbar-logo-container">
+            <Link to="/">
+                <img
+                    alt="Codeblogs Website Logo"
+                    aria-label="Codeblogs Logo"
+                    className="nav-logo-image"
+                    src={logoSrc}
+                    loading="lazy"
+                />
+            </Link>
+        </div>
+    );
+});
 
 Logo.displayName = 'Logo';
+
+export default Logo;
